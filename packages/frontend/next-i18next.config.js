01ce9e5b2385090b66484a4f14bfc0ebc6d6b97f
Samuel <esamuel55@hotmail.com>
2025-10-30 10:55:20 +0200

    fix(frontend): set i18n.localeDetection to boolean false for Next.js 14

--- packages/frontend/next-i18next.config.js
+++ packages/frontend/next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'he', 'es'],
-    localeDetection: true,
+    localeDetection: false,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
